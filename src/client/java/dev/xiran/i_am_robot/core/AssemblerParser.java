5eb61ebfc5a1f7744244f1b0cdbe0790e67b0e91
xiran-ya <xiran23353456@163.com>
2026-04-03 21:43:44 +0800

    Change the way of parsing arguments

--- src/client/java/dev/xiran/i_am_robot/core/AssemblerParser.java
+++ src/client/java/dev/xiran/i_am_robot/core/AssemblerParser.java
@@ -4,52 +4,54 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 
 import java.util.IllegalFormatException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 一个脚本解释器，使用简易的类汇编语言
  */
 public class AssemblerParser {
+    public static final Pattern stringPattern = Pattern.compile("\".*\"");
+
     public static boolean evaluate(String instruction) throws SyntaxException {
         if (instruction == null) {
             PlayerActionUtil.sendClientMessage(Component.translatable("message.i_am_robot.vm.null_instruction").withStyle(ChatFormatting.GOLD, ChatFormatting.ITALIC));
             return false;
         }
 
-        String[] tokens = instruction.trim().split(" ");
+        String[] tokens = instruction.trim().split(" +");
         try {
             switch (tokens[0]) {
-                case "int":
+                case "int" -> {
                     VirtualMachine.INSTANCE.createVariable(tokens[1], Integer.parseInt(tokens[2]));
                     return true;
-                case "double":
+                }
+                case "double" -> {
                     VirtualMachine.INSTANCE.createVariable(tokens[1], Double.parseDouble(tokens[2]));
                     return true;
-                case "string":
-                case "String":
-                    StringBuilder builder = new StringBuilder();
-                    for (int i = 2; i < tokens.length; i++) {
-                        builder.append(tokens[i]);
-                        builder.append(' ');
-                    }
-                    if (!builder.isEmpty()) builder.deleteCharAt(builder.length() - 1);
-                    VirtualMachine.INSTANCE.createVariable(tokens[1], builder.toString());
+                }
+                case "string", "String" -> {
+                    Matcher matcher = stringPattern.matcher(instruction);
+                    if (!matcher.find()) throw new SyntaxException("Missing argument, string expected");
+                    VirtualMachine.INSTANCE.createVariable(tokens[1], instruction.substring(matcher.start() + 1, matcher.end() - 1));
                     return true;
-                case "mov":
-                    Object sourceObj = VirtualMachine.INSTANCE.getVariable(tokens[1]);
+                }
+                case "mov" -> {
+                    Object sourceObj;
+                    if (tokens[1].charAt(0) == '"') {
+                        Matcher matcher = stringPattern.matcher(instruction);
+                        if (!matcher.find()) throw new SyntaxException("Missing argument, string expected");
+                        sourceObj = instruction.substring(matcher.start() + 1, matcher.end() - 1);
+                    } else {
+                        sourceObj = parseValue(tokens[1]);
+                    }
+
                     VirtualMachine.INSTANCE.setVariable(tokens[2], sourceObj);
                     return true;
-                case "add":
+                }
+                case "add" -> {
                     Object var0 = VirtualMachine.INSTANCE.getVariable(tokens[1]);
-                    Object var1;
-                    if (tokens[2].matches("^[0-9+-].*")) {
-                        if (tokens[2].contains(".")) {
-                            var1 = Double.parseDouble(tokens[2]);
-                        } else {
-                            var1 = Integer.parseInt(tokens[2]);
-                        }
-                    } else {
-                        var1 = VirtualMachine.INSTANCE.getVariable(tokens[2]);
-                    }
+                    Object var1 = parseValue(tokens[2]);
 
                     try {
                         if (var0 instanceof Integer) {
@@ -69,13 +71,14 @@ public class AssemblerParser {
                     } catch (ClassCastException e) {
                         throw new VMRuntimeException("Cannot add with none-number variable");
                     }
-                case "format":
+                }
+                case "format" -> {
                     Object format = VirtualMachine.INSTANCE.getVariable(tokens[1]);
                     if (format instanceof String) {
                         try {
                             Object[] formatArgs = new Object[tokens.length - 2];
                             for (int i = 0; i < tokens.length - 2; i++) {
-                                formatArgs[i] = VirtualMachine.INSTANCE.getVariable(tokens[i + 2]);
+                                formatArgs[i] = parseValue(tokens[1 + 2]);
                             }
                             String formattedString = String.format((String) format, formatArgs);
                             VirtualMachine.INSTANCE.setVariable(tokens[1], formattedString);
@@ -86,20 +89,39 @@ public class AssemblerParser {
                     } else {
                         throw new VMRuntimeException("Format can only be used on string variable");
                     }
-                case "log":
-                    Object obj = VirtualMachine.INSTANCE.getVariable(tokens[1]);
-                    PlayerActionUtil.sendClientMessage(Component.literal(obj.toString()));
+                }
+                case "log" -> {
+                    PlayerActionUtil.sendClientMessage(Component.literal(parseValue(tokens[1]).toString()));
                     return true;
-                case "halt":
+                }
+                case "halt" -> {
                     PlayerActionUtil.sendClientMessage(Component.translatable("message.i_am_robot.vm.halt").withStyle(ChatFormatting.GRAY, ChatFormatting.ITALIC));
                     return false;
-                default:
-                    throw new SyntaxException("Invalid keyword");
+                }
+                default -> throw new SyntaxException("Invalid keyword");
             }
         } catch (ArrayIndexOutOfBoundsException e) {
             throw new SyntaxException("Instruction is missing argument");
         } catch (NumberFormatException e) {
             throw new SyntaxException("Invalid number format");
+        }
+    }
+
+    /**
+     * 解析字符串对应的值
+     * @param string 输入字符串，可以是字面量或标识符
+     * @return 字符串对应的值
+     */
+    private static Object parseValue(String string) {
+        char c = string.charAt(0);
+        if (Character.isDigit(c) || c == '+' || c == '-') {
+            if (string.contains(".")) {
+                return Double.parseDouble(string);
+            } else {
+                return Integer.parseInt(string);
+            }
+        } else {
+            return VirtualMachine.INSTANCE.getVariable(string);
         }
     }
 }
