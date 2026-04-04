36abdeb1defee303ce14bd42c6dd27bc127ded98
xiran-ya <xiran23353456@163.com>
2026-04-04 15:55:48 +0800

    Use ArrayDeque for messageQueue

--- src/client/java/dev/xiran/i_am_robot/core/PlayerActionUtil.java
+++ src/client/java/dev/xiran/i_am_robot/core/PlayerActionUtil.java
@@ -3,11 +3,12 @@ package dev.xiran.i_am_robot.core;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class PlayerActionUtil {
 
-    public static LinkedList<Component> messageQueue = new LinkedList<>();
+    public static Deque<Component> messageQueue = new ArrayDeque<>();
 
     public static void sendClientMessage(Component component) {
         messageQueue.addLast(component);
@@ -16,6 +17,7 @@ public class PlayerActionUtil {
     // 在非 Render Thread 发送消息会莫名其妙抛异常，所以只能让 Render Thread 在 tick 时发送
     public static void sendMessageOnTick(Minecraft mc) {
         if (!messageQueue.isEmpty()) {
+            //noinspection DataFlowIssue
             mc.gui.getChat().addMessage(messageQueue.poll());
         }
     }
