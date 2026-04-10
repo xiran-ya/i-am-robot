a757fd37ef0f55b4f1d16920db32c226ae2caa9d
xiran-ya <xiran23353456@163.com>
2026-04-10 23:31:02 +0800

    Fix: use ReentrantLock to ensure synchronization

--- src/client/java/dev/xiran/i_am_robot/core/PlayerActionUtil.java
+++ src/client/java/dev/xiran/i_am_robot/core/PlayerActionUtil.java
@@ -5,20 +5,32 @@ import net.minecraft.network.chat.Component;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class PlayerActionUtil {
 
     public static Deque<Component> messageQueue = new ArrayDeque<>();
+    private static final ReentrantLock queueLock = new ReentrantLock();
 
     public static void sendClientMessage(Component component) {
-        messageQueue.addLast(component);
+        queueLock.lock();
+        try {
+            messageQueue.addLast(component);
+        } finally {
+            queueLock.unlock();
+        }
     }
 
     // 在非 Render Thread 发送消息会莫名其妙抛异常，所以只能让 Render Thread 在 tick 时发送
     public static void sendMessageOnTick(Minecraft mc) {
-        if (!messageQueue.isEmpty()) {
-            //noinspection DataFlowIssue
-            mc.gui.getChat().addMessage(messageQueue.poll());
+        queueLock.lock();
+        try {
+            if (!messageQueue.isEmpty()) {
+                //noinspection DataFlowIssue
+                mc.gui.getChat().addMessage(messageQueue.poll());
+            }
+        } finally {
+            queueLock.unlock();
         }
     }
 }
